612c7ce565da43667a994f8848ab213cf66d1011
Saham Nadeem <apple@192.168.10.19>
2023-07-08 23:59:33 +0500

    update visibility

--- app/Http/Controllers/Api/v1/HomeController.php
+++ app/Http/Controllers/Api/v1/HomeController.php
@@ -86,6 +86,7 @@ class HomeController extends Controller
             $r->where('category_id',$id);
         })->where('status',1)->get();
         $result = [];
+        dd($stores);
         foreach($stores as $store){
             $available_stories = [];
             foreach($store->available_stories as $story){
